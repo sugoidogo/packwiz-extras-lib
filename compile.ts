9302381e793a6f5f0e3dc03f609b048064e3060b
sugoidogo <sugoidogo@sugoidogo.com>
2025-11-17 05:09:54 -0800

    update compile and script targets

--- compile.ts
+++ compile.ts
@@ -1,19 +1,33 @@
-const targets = [
+import { spawnSync } from "node:child_process"
+
+const deno_targets = [
     'x86_64-apple-darwin',
     'aarch64-apple-darwin',
     'x86_64-pc-windows-msvc',
     'x86_64-unknown-linux-gnu',
     'aarch64-unknown-linux-gnu'
 ]
-for (const target of targets) {
-    const status = await new Deno.Command('deno', { args: ['check', 'main.ts'] }).spawn().status
-    if (!status.success) {
-        Deno.exit(status.code)
-    }
-    new Deno.Command('deno', {
-        args: [
-            'compile', '-P', '--target', target,
-            '--output', 'dist/packwiz-util-' + target,
-            '--no-check','main.ts']
-    }).spawn().ref()
+for (const target of deno_targets) {
+    spawnSync('deno', [
+        'compile', '-P', '--target', target,
+        '--output', 'dist/deno/packwiz-util-' + target,
+        '--no-check', 'main.ts'
+    ], { stdio: 'inherit' })
+}
+const bun_targets = [
+    'bun-linux-x64-baseline',
+    'bun-linux-arm64',
+    'bun-linux-x64-musl-baseline',
+    'bun-linux-arm64-musl',
+    'bun-windows-x64-baseline',
+    'bun-darwin-x64-baseline',
+    'bun-darwin-arm64'
+]
+for (const target of bun_targets) {
+    spawnSync('bun', [
+        'build', '--compile',
+        '--target', target,
+        '--outfile', 'dist/bun/packwiz-util-' + target,
+        'main.ts'
+    ], { stdio: 'inherit' })
 }
