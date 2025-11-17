18b45625bb9fe8d1b4a15ba542435135dfa8f633
sugoidogo <sugoidogo@sugoidogo.com>
2025-11-16 23:44:52 -0800

    fix test

--- test.ts
+++ test.ts
@@ -1,4 +1,7 @@
-import { existsSync } from "jsr:@std/fs/exists";
+import { existsSync } from "@std/fs/exists"
+import * as dotenv from '@std/dotenv'
+
+dotenv.loadSync({ export: true })
 if (!existsSync('modpack.zip')) {
     console.log('downloading test modpack...')
     const response = await fetch('https://mediafilez.forgecdn.net/files/7223/56/All%20the%20Mods%2010-5.1.zip')
@@ -8,7 +11,8 @@ if (!existsSync('modpack.zip')) {
 Deno.mkdirSync('test', { recursive: true })
 Deno.chdir('test')
 await new Deno.Command('packwiz', { args: ['curseforge', 'import', '../modpack.zip'] }).spawn().status
-await new Deno.Command('deno', {args: ['run', '-P', '../main.ts','--cf-detect','--cf-url','--mr-merge']}).spawn().status
-await new Deno.Command('packwiz', { args: ['modrinth', 'export'] }).spawn().status
+const status = await new Deno.Command('deno', { args: ['run', '-P', '../main.ts', '--cf-detect', '--cf-url', '--mr-merge'] }).spawn().status
+//await new Deno.Command('packwiz', { args: ['modrinth', 'export'] }).spawn().status
 Deno.chdir('..')
-Deno.removeSync('test',{recursive:true})
+Deno.removeSync('test', { recursive: true })
+Deno.exit(status.code)
